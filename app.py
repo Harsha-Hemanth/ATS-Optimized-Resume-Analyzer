9511bdd590fb0578000a59a3fde5fc80cebe1855
Harsha-Hemanth <158434803+Harsha-Hemanth@users.noreply.github.com>
2024-09-14 11:44:00 +0530

    integrated gemini-pro model with web frame work

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def input_pdf_text(uploaded_file):
     text = ''
     for page_num in range(len(reader.pages)):
         page = reader.pages[page_num]
-        text += str(extract_text(page))
+        text += str(page.extract_text())
     return text
 
 input_prompt="""
@@ -46,3 +46,99 @@ The third section provides a profile summary.
 Mention the title for all the three sections.
 While generating the response put some space to separate all the three sections.
 """
+
+# Page configuration
+st.set_page_config(page_title="Resume ATS Tracker", layout="wide")
+
+# Introduction Section
+def introduction():
+    st.write("")  # Adding vertical space
+    col1, col2 = st.columns([3, 2])
+
+    with col1:
+        st.title("CareerCraft")
+        st.header("Navigate the Job Market with Confidence!")
+        st.markdown("""
+        <p style="text-align: justify;">
+        Introducing CareerCraft, an ATS-Optimized Resume Analyzer - your ultimate solution for optimizing job applications and accelerating career growth. 
+        Our innovative platform leverages advanced ATS technology to provide job seekers with valuable insights into their resumes' compatibility with job descriptions. 
+        From resume optimization and skill enhancement to career progression guidance, CareerCraft empowers users to stand out in today's competitive job market.
+        Streamline your job application process, enhance your skills, and navigate your career path with confidence. Join CareerCraft today and unlock new opportunities for professional success!
+        </p>
+        """, unsafe_allow_html=True)
+
+    with col2:
+        st.image('images/introduction_image.gif', use_column_width=True)
+    st.write("")  # Adding vertical space
+
+# Offerings Section
+def offerings():
+    st.write("")  # Adding vertical space
+    col1, col2 = st.columns([3, 2])
+
+    with col1:
+        st.image('images/offerings_image.png', use_column_width=True)
+
+    with col2:
+        st.header("Wide Range of Offerings")
+        st.write("""
+        - **ATS-Optimized Resume Analysis**
+        - **Resume Optimization**
+        - **Skill Enhancement**
+        - **Career Progression Guidance**
+        - **Interview Preparation**
+        - **Job Application Strategy**
+        """)
+    st.write("")  # Adding vertical space
+
+st.markdown("""
+    <style>
+        .stTextArea textarea {
+            background-color: #228a27;
+        }
+        [data-testid="stFileUploaderDropzone"] {
+            background-color: #228a27;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Resume ATS Tracking Application Section
+def resume_ats_tracking():
+    col1, col2 = st.columns([3, 2]) 
+    with col1: 
+        st.markdown("<h1 style='text-align: center;'>Embark on Your Career Adventure</h1>", unsafe_allow_html=True) 
+        jd=st.text_area("Paste the Job Description") 
+        uploaded_file=st.file_uploader("Upload Your Resume", type="pdf", help="Please uplaod the pdf") 
+        submit = st.button("Submit") 
+        if submit: 
+            if uploaded_file is not None: 
+                text=input_pdf_text(uploaded_file) 
+                response=get_gemini_response (input_prompt) 
+                st.subheader(response) 
+
+    with col2: 
+        st.image("./images/application_image.png", use_column_width=True) 
+        st.write("")
+
+# FAQ Section
+def faq():
+    col1, col2 = st.columns([2, 3]) 
+    with col2: 
+        st.markdown("<h1 style='text-align: center;'>FAQ</h1>", unsafe_allow_html=True) 
+        st.write("Question: How does CareerCraft analyze resumes and job descriptions?") 
+        st.write("""Answer: CareerCraft uses advanced algorithms to analyze resumes and job descriptions, identifying key keywords and assessing compatibility between the two.""") 
+        st.write("")
+        st.write("Question: Can CareerCraft suggest improvements for my resume?") 
+        st.write("""Answer: Yes, CareerCraft provides personalized recommendations to optimize your resume for specific job openings, including suggestions for missing keywords and alignment with desired job roles.""") 
+        st.write("") 
+        st. write("Question: Is CareerCraft suitable for both entry-level and experienced professionals?") 
+        st.write("""Answer: Absolutely! CareerCraft caters to job seekers at all career stages, offering tailored insights and guidance to enhance their resumes and advance their careers.""") 
+    
+    with col1: 
+        st.image("./images/faq_image.png", use_column_width=True)
+
+# Render the sections
+introduction()
+offerings()
+resume_ats_tracking()
+faq()
